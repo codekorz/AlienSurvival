2786eb0e11eebfd06f3e7680a7116cdf4e1c016f
Marisa <Marisa@Marisa.com>
2016-01-13 19:13:22 +0100

    Rango busqueda hecho

--- AlienSurvival/Assets/[Game]/Scripts/AlienController.cs
+++ AlienSurvival/Assets/[Game]/Scripts/AlienController.cs
@@ -3,41 +3,36 @@ using System.Collections;
 
 public class AlienController : MonoBehaviour {
 
-    private NavMeshHit m_navhit;
+
+    private RangeController m_range;
+    //private NavMeshHit m_navhit;
     private NavMeshAgent m_agent;
     private Animator m_animator;
-   // private Rigidbody m_rigidbody;
+    private Rigidbody m_rigidbody;
     private Vector3 m_moveDirection = Vector3.zero;
     public float m_speed = 6f;
     public float m_gravity = 9.8f;
     public GameObject target;
+    //private NavMeshAgent[] m_waypoints;
+
     // Use this for initialization
     void Start () {
         m_animator = GetComponent<Animator>();
         //m_rigidbody = GetComponent<Rigidbody>();
+        m_range = transform.FindChild("Sensor").GetComponent<RangeController>();
     }
 	
 	// Update is called once per frame
 	void Update () {
-        m_agent = GetComponent<NavMeshAgent>();
-        m_agent.destination = target.transform.position;
-        Debug.Log("Position:" + target.transform.position);
-       // transform.Rotate(0, Input.GetAxisRaw("Horizontal") * 60 * Time.deltaTime, 0);
-       // float horizontal = Input.GetAxisRaw("Horizontal");
-       // float vertical = Input.GetAxisRaw("Vertical");
-       // Move(horizontal, vertical);
+
+        if (m_range.getRange())
+        {
+            Debug.Log("HE RECIBIDO UN PERSONAJEEE EN MI RANGOOO");
+            transform.Translate(new Vector3(0, 20, 0));
+            //m_agent = GetComponent<NavMeshAgent>();
+            //m_agent.destination = target.transform.position;
+        }
+        //Debug.Log("Position:" + target.transform.position);
     }
 
-    void Move(float h, float v)
-    {
-       // m_moveDirection = new Vector3(h, 0, v);
-       // if (m_moveDirection != Vector3.zero)
-       // {
-       //     m_moveDirection = transform.TransformDirection(m_moveDirection);
-       //     m_moveDirection *= m_speed;
-          //  m_moveDirection.y -= m_gravity * Time.deltaTime;
-       //     m_rigidbody.AddForce(m_moveDirection * Time.deltaTime);
-       //     Debug.Log("H" + h + "-V:" + v);
-       // }
-    }
 }
---
+++ AlienSurvival/Assets/[Game]/Scripts/RangeController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RangeController : MonoBehaviour {
+
+    // Use this for initialization
+
+    private bool m_inrange;
+
+
+    public bool getRange()
+    {
+        return m_inrange;
+    }
+
+    void Start () {
+        m_inrange = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider collider) {
+
+        if (collider.gameObject.name == "MainCharacter")
+        {
+            Debug.Log("Ha entrado el personaje");
+            m_inrange = false;
+        }
+    }
+
+    void OnTriggerStay(Collider collider)
+    {
+        if (collider.gameObject.name == "MainCharacter")
+        {
+            Debug.Log("Está el personaje en el rango del Alien");
+            m_inrange = true;
+        }
+    }
+
+    void OnTriggerExit(Collider collider)
+    {
+        if (collider.gameObject.name == "MainCharacter")
+        {
+            Debug.Log("Ha salido el personaje");
+            m_inrange = false;
+        }
+    }
+
+}

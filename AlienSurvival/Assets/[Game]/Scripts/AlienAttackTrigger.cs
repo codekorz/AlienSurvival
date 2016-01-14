3e87daf346d56c238bb087f191d14a6534389573
Ismael Piñeiro Ramos <Ismael Pieiro Ramos>
2016-01-14 22:43:12 +0100

    fixing conflicts, isma

---
+++ AlienSurvival/Assets/[Game]/Scripts/AlienAttackTrigger.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AlienAttackTrigger : MonoBehaviour {
+	void OnTriggerEnter(Collider collider)
+	{
+		Debug.Log ("Collision");
+		if(collider.tag == "Player")
+			Destroy(collider.gameObject);
+	}
+
+}
--- AlienSurvival/Assets/[Game]/Scripts/AlienController.cs
+++ AlienSurvival/Assets/[Game]/Scripts/AlienController.cs
@@ -2,38 +2,35 @@
 using System.Collections;
 
 public class AlienController : MonoBehaviour {
-
-    private RangeController m_range;
-    //private NavMeshHit m_navhit;
+   
+	private RangeController m_range;
     private NavMeshAgent m_agent;
     private Animator m_animator;
-    private Rigidbody m_rigidbody;
-    private Vector3 m_moveDirection = Vector3.zero;
-    public float m_speed = 6f;
-    public float m_gravity = 9.8f;
+	private bool m_attack;
+
     public Transform m_target;
-    //private NavMeshAgent[] m_waypoints;
 
-    // Use this for initialization
     void Start () {
+		m_attack = false;
         m_animator = GetComponent<Animator>();
-        //m_rigidbody = GetComponent<Rigidbody>();
         m_range = transform.FindChild("Sensor").GetComponent<RangeController>();
-        //m_target = GameObject.Find("Player").GetComponent<Transform>();
+		m_agent = GetComponent<NavMeshAgent>();
     }
-	
-	// Update is called once per frame
-	void Update () {
 
-        if (m_range.getRange())
-        {
-            Debug.Log("HE RECIBIDO UN PERSONAJEEE EN MI RANGOOO");
-  
-            //transform.Translate(new Vector3(0, 20, 0));
-            m_agent = GetComponent<NavMeshAgent>();
-            m_agent.destination = m_target.position;
+	void FixedUpdate () {
+		m_animator.SetFloat ("speed", m_agent.speed);
+		if (m_range.getRange ()) {  	
+			if(!m_attack)
+				m_agent.destination = m_target.position;
+			transform.LookAt (m_target);
+			if (m_agent.remainingDistance < 5 && !m_animator.GetCurrentAnimatorStateInfo (0).IsName ("Attack")){
+				m_attack = true;
+			}else{
+				m_attack = false;
+			}
+			m_animator.SetBool ("attack", m_attack);
         }
-        //Debug.Log("Position:" + target.transform.position);
     }
+
 
 }

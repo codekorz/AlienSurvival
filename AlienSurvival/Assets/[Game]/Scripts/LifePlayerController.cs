79a91cedb9619b552cdb9cebf93daca09bf49732
Marisa <Marisa@Marisa.com>
2016-01-15 21:13:57 +0100

    Game Over v1

--- AlienSurvival/Assets/[Game]/Scripts/LifePlayerController.cs
+++ AlienSurvival/Assets/[Game]/Scripts/LifePlayerController.cs
@@ -4,25 +4,39 @@ using UnityEngine.SceneManagement;
 using System.Collections;
 
 public class LifePlayerController : MonoBehaviour {
-
-
+    private AsyncOperation async;
     public int vidasIniciales = 3;
-    public int vidasActuales = 0;
+    public int vidasActuales = -1;
     public int puntuacion = 0;
     public RawImage[] heart;
-    
+    public RawImage gameOver;
 	// Use this for initialization
 	void Start () {
+        Time.timeScale = 1;
         vidasActuales = vidasIniciales;
         puntuacion = 0;
         heart = GameObject.Find("Lifes").GetComponentsInChildren<RawImage>();
-       // corazones = Texture2D.get        
-	}
+        //gameOver = GameObject.Find("GameOver").GetComponent<RawImage>();
+        //gameOver.enabled = false;
+        
+        // corazones = Texture2D.get        
+    }
 	
 	// Update is called once per frame
 	void Update () {
         if (vidasActuales == 0)
-            SceneManager.LoadScene("GameScene");
+        {
+            Time.timeScale = 0;
+            //gameOver.enabled = true;
+            //async = SceneManager.LoadSceneAsync("GameScene");
+            //async.allowSceneActivation = false;
+            if (Input.anyKey /*|| async.progress >= 0.9*/)
+            {
+                //async.allowSceneActivation = true;
+                SceneManager.LoadScene("GameScene");
+            }
+
+        }
     }
 
     public void perderVida()
